bccee63e0328ee5871c8ea4b908e441bd765cb5f
Riccy-AI <riccywidjhaya00@gmail.com>
2025-10-15 13:00:20 +0700

    Menghapus direktori absensiApp/absensi yang tidak terpakai.

--- android/absensiApp/absensi/web_entrypoint.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart'; // Import Flutter Material package
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
-import 'lib/main.dart'; // Correct the import path
-
-void main() {
-  setUrlStrategy(PathUrlStrategy());
-  runApp(const MyApp()); // Use runApp to start the application
-}
